70cfb726e88ada2c290689bd0dcd1beb71ddb507
frontendgod <3054750147@qq.com>
2020-01-25 20:43:52 +0800

    p5 done

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
+import store from './store'
 
-const data = [
-    '早8点开晨会，分配今天的任务',
-    '早9点和项目经理开需求沟通会',
-    '早9点和项目经理开需求沟通会',
-]
 
 class TodoList extends Component {
+
+    constructor(props){
+        super(props)
+        console.log(store.getState())
+        this.state = store.getState()
+    }
 
     render() {
 		return (
 			<div style={{margin:'10px'}}>
                 <div>
                     <Input 
-                        placeholder="wirte something"
+                        placeholder={this.state.inputValue}
                         style={{width:'250px',marginRight:'10px'}} 
                     />
                     <Button 
@@ -25,7 +27,7 @@ class TodoList extends Component {
                 <div style={{margin: '10px',width:'300px'}}>
                     <List 
                         bordered
-                        dataSource={data}
+                        dataSource={this.state.list}
                         renderItem={item =>(<List.Item>{item}</List.Item>)}
                     />
                 </div>
---
+++ src/store/reducer.js
@@ -0,0 +1,11 @@
+const defaultState = {
+    inputValue: 'Write Something',
+    list: [
+        '早8点开晨会，分配今天的任务',
+        '早9点和项目经理开需求沟通会',
+        '晚5点进行代码review',
+    ]
+}
+export default (state=defaultState, action)=>{
+    return state
+}
