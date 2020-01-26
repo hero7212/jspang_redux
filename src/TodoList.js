f694936d26af5e199ab5d789445e34f80690f031
frontendgod <3054750147@qq.com>
2020-01-26 15:15:28 +0800

    p11 done

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 import store from './store'
-import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from './store/actionTypes'
+import { changeInputAction, addItemAction, deleteItemAction } from './store/actionCreators'
 
 
 class TodoList extends Component {
@@ -48,23 +48,15 @@ class TodoList extends Component {
     }
 
     changeInputValue(e) {
-        const action = {
-            type: CHANGE_INPUT,
-            value: e.target.value
-        }
+        const action = changeInputAction(e.target.value)
         store.dispatch(action)
     }
-
-
     clickBtn() {
-        const action = {type: ADD_ITEM}
+        const action = addItemAction()
         store.dispatch(action)
     }
     deleteItem(index) {
-        const action = {
-            type: DELETE_ITEM,
-            index
-        }
+        const action = deleteItemAction(index)
         store.dispatch(action)
     }
 }
